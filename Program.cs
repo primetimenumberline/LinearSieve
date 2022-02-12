82f76d147b9d99094674041f79af1cf21078831c
primetimenumberline <primetimenumberline@gmail.com>
2022-02-12 16:42:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int n = 101;
+bool[] b = new bool[n + 1];
+List<int> S = new List<int>();
+for (int i = 2; i <= n; i++)
+{
+    if (!b[i])
+        S.Add(i);
+    foreach (int j in S)
+    {
+        int k = i * j;
+        if (k > n) break;
+        b[k] = true;
+        if (i % j == 0) break;
+    }
+}
+foreach(int i in S)
+    Console.WriteLine(i);
